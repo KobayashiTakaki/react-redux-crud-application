f8cd128e7bffdeb3b5cbe61db57ccec7731a4723
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-10-10 22:04:09 +0900

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log('clicked!')}} />
-      </React.Fragment>
-    )
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log('clicked!')}} />
+//       </React.Fragment>
+//     )
+//
+//
+//   }
+// }
 
+const App = () => {
+  return (
+    <div>
+    Hello!
+    <Cat />
+    <Cat />
+    <Cat />
+    </div>
+  )
 
-  }
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 // class App extends Component {
