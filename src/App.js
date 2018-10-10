88d81feb0bf397d3c044b9c93e881c35464dcca3
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-10-10 21:41:02 +0900

    change message h1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello, World!</h1>
+    );
+  }
+}
+
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello World!!"
+//     );
+//   }
+// }
+export default App;
